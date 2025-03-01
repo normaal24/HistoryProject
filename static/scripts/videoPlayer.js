f9e1d02cd641520bbee67d5bf0f76a6e2b53b3dd
Speedy35462 <sheptukham@gmail.com>
2025-03-01 20:44:38 +0200

    Додав відео до Дейнеги

---
+++ static/scripts/videoPlayer.js
@@ -0,0 +1,27 @@
+let videoPlayers = document.getElementsByClassName("video");
+
+for (let i = 0; i < videoPlayers.length; i++) {
+    let playButton = document.createElement("button");
+    playButton.classList.add("play-button");
+
+    let video = videoPlayers[i].getElementsByTagName("video")[0];
+
+    video.addEventListener("click", () => {
+        if (!video.paused) {
+            video.pause();
+            playButton.style.display = "block";
+        }
+    });
+
+    playButton.addEventListener("click", () => {
+        if (video.paused) {
+            video.play();
+            playButton.style.display = "none";
+        } else {
+            video.pause();
+            playButton.style.display = "block";
+        }
+    });
+
+    videoPlayers[i].appendChild(playButton);
+}
